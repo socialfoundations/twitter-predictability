9eff3ea04c8293a35e5debdf3edbf4d650b1375a
Mina Remeli <mina.remeli@tuebingen.mpg.de>
2023-02-16 16:46:07 +0100

    Twitter object model converter + tests

---
+++ data_collection/converter.py
@@ -0,0 +1,99 @@
+# Converts V2 Twitter API object models to V1.1
+from jsonbender import K, S, F, OptionalS, bend
+from jsonbender.list_ops import ForallBend
+from datetime import datetime
+
+
+def v2_to_v1_date(datestr):
+    d = datetime.fromisoformat(datestr.replace("Z", "+00:00"))
+    return d.strftime("%a %b %d %X %z %Y")
+
+
+# Filled fields with default value:
+#   - favourites_count
+V2_TO_V1_USER = {
+    "id": S("data", "id") >> F(int),
+    "id_str": S("data", "id"),
+    "name": S("data", "name"),
+    "screen_name": S("data", "username"),
+    "location": OptionalS("data", "location"),
+    "description": S("data", "description"),
+    "entities": {
+        "url": {
+            "urls": OptionalS("data", "entities", "url", "urls", default=[])
+            >> ForallBend(
+                {
+                    "url": S("url"),
+                    "expanded_url": S("expanded_url"),
+                    "display_url": S("display_url"),
+                    "indices": [S("start"), S("end")],
+                }
+            ),
+        },
+    },
+    "url": OptionalS("data", "url"),
+    "protected": S("data", "protected"),
+    "followers_count": S("data", "public_metrics", "followers_count"),
+    "friends_count": S("data", "public_metrics", "following_count"),
+    "listed_count": S("data", "public_metrics", "listed_count"),
+    "favourites_count": K("0") >> F(int),
+    "statuses_count": S("data", "public_metrics", "tweet_count"),
+    "created_at": S("data", "created_at") >> F(v2_to_v1_date),
+    "verified": S("data", "verified"),
+    "profile_image_url_https": S("data", "profile_image_url"),
+}
+
+
+def bend_user(user):
+    user = {"data": user}
+    return bend(mapping=V2_TO_V1_USER, source=user)
+
+
+# Filled fields with default value:
+#   - entities.user_mentions.id
+#   - entities.user_mentions.id_str
+V2_TO_V1_TWEET = {
+    "created_at": S("data", "created_at") >> F(v2_to_v1_date),
+    "id": S("data", "id") >> F(int),
+    "id_str": S("data", "id"),
+    "text": S("data", "text"),
+    "entities": {
+        "user_mentions": OptionalS("data", "entities", "mentions", default=[])
+        >> ForallBend(
+            {
+                "screen_name": S("tag"),
+                "id": K("0") >> F(int),
+                "id_str": K("0"),
+                "indices": [S("start"), S("end")],
+            }
+        ),
+        "symbols": OptionalS("data", "entities", "cashtags", default=[])
+        >> ForallBend(
+            {
+                "text": S("tag"),
+                "indices": [S("start"), S("end")],
+            }
+        ),
+        "hashtags": OptionalS("data", "entities", "hashtags", default=[])
+        >> ForallBend(
+            {
+                "text": S("tag"),
+                "indices": [S("start"), S("end")],
+            }
+        ),
+        "urls": OptionalS("data", "entities", "urls", default=[])
+        >> ForallBend(
+            {
+                "url": S("url"),
+                "expanded_url": S("expanded_url"),
+                "display_url": S("display_url"),
+                "indices": [S("start"), S("end")],
+            }
+        ),
+    },
+    "possibly_sensitive": S("data", "possibly_sensitive"),
+    "lang": S("data", "lang"),
+    "user": OptionalS("includes", "users")[0] >> F(bend_user),
+    "retweet_count": S("data", "public_metrics", "retweet_count"),
+    "favorite_count": S("data", "public_metrics", "like_count"),
+}
---
+++ test/test_converter.py
@@ -0,0 +1,150 @@
+import unittest
+import os
+from data_collection.converter import V2_TO_V1_TWEET, V2_TO_V1_USER
+from jsonbender import bend
+import json
+from jsoncomparison import Compare, NO_DIFF
+
+FILE_PATH = os.path.dirname(__file__)
+TEST_FILES_PATH = os.path.join(FILE_PATH, "converter_examples")
+DIFFS_PATH = os.path.join(FILE_PATH, "converter_diffs")
+
+if not os.path.exists(DIFFS_PATH):
+    os.makedirs(DIFFS_PATH)
+
+compare_config = {
+    "output": {
+        "console": False,
+        "file": {
+            "allow_nan": True,
+            "ensure_ascii": True,
+            "indent": 4,
+            "name": os.path.join(FILE_PATH, "difference.json"),
+            "skipkeys": True,
+        },
+    }
+}
+
+# ignore these keys when comparing
+user_exceptions = {
+    "id": "*",  # V1 stored a 53-bit int representation of the 64-bit id, which can cause a mismatch between v2_id and v1_id
+    "favourites_count": "*",
+    "profile_location": "*",
+    "entities": {"description": "*"},
+    "utc_offset": "*",
+    "time_zone": "*",
+    "geo_enabled": "*",
+    "lang": "*",
+    "status": "*",
+    "contributors_enabled": "*",
+    "is_translator": "*",
+    "is_translation_enabled": "*",
+    "profile_background_color": "*",
+    "profile_background_image_url": "*",
+    "profile_background_image_url_https": "*",
+    "profile_background_tile": "*",
+    "profile_image_url": "*",
+    "profile_banner_url": "*",
+    "profile_link_color": "*",
+    "profile_sidebar_border_color": "*",
+    "profile_sidebar_fill_color": "*",
+    "profile_text_color": "*",
+    "profile_use_background_image": "*",
+    "has_extended_profile": "*",
+    "default_profile": "*",
+    "default_profile_image": "*",
+    "following": "*",
+    "follow_request_sent": "*",
+    "notifications": "*",
+    "translator_type": "*",
+    "withheld_in_countries": "*",
+}
+
+tweet_exceptions = {
+    "id": "*",  # V1 stored a 53-bit int representation of the 64-bit id, which can cause a mismatch between v2_id and v1_id
+    "truncated": "*",
+    "source": "*",
+    "in_reply_to_status_id": "*",
+    "in_reply_to_status_id_str": "*",
+    "in_reply_to_user_id": "*",
+    "in_reply_to_user_id_str": "*",
+    "in_reply_to_screen_name": "*",
+    "user": user_exceptions,
+    "geo": "*",
+    "coordinates": "*",
+    "place": "*",
+    "contributors": "*",
+    "is_quote_status": "*",
+    "quoted_status_id": "*",
+    "quoted_status_id_str": "*",
+    "quoted_status": "*",
+    "favorited": "*",
+    "retweeted": "*",
+    "possibly_sensitive_appealable": "*",
+}
+
+
+class TestUserConverters(unittest.TestCase):
+    def setUp(self):
+        self.compare_config = compare_config
+        self.compare_config["output"]["file"]["name"] = os.path.join(
+            DIFFS_PATH, self.id() + ".json"
+        )
+
+    def _get_expected_actual(self, file1, file2):
+        with open(os.path.join(TEST_FILES_PATH, file1)) as file:
+            v2_user = json.load(file)
+        v1_user_actual = bend(V2_TO_V1_USER, v2_user)
+        with open(os.path.join(TEST_FILES_PATH, file2)) as file:
+            v1_user_expected = json.load(file)
+        return v1_user_expected, v1_user_actual
+
+    def test_V2_to_V1_user_example(self):
+        expected, actual = self._get_expected_actual("V2_user.json", "V1_user.json")
+        diff = Compare(config=self.compare_config, rules=user_exceptions).check(
+            expected, actual
+        )
+        self.assertEqual(diff, NO_DIFF)
+
+    def test_V2_to_V1_user_example_1(self):
+        expected, actual = self._get_expected_actual("V2_user_1.json", "V1_user_1.json")
+        diff = Compare(config=self.compare_config, rules=user_exceptions).check(
+            expected, actual
+        )
+        self.assertEqual(diff, NO_DIFF)
+
+
+class TestTweetConverters(unittest.TestCase):
+    def setUp(self):
+        self.compare_config = compare_config
+        self.compare_config["output"]["file"]["name"] = os.path.join(
+            DIFFS_PATH, self.id() + ".json"
+        )
+
+    def _get_expected_actual(self, file1, file2):
+        with open(os.path.join(TEST_FILES_PATH, file1)) as file:
+            v2_user = json.load(file)
+        v1_user_actual = bend(V2_TO_V1_TWEET, v2_user)
+        with open(os.path.join(TEST_FILES_PATH, file2)) as file:
+            v1_user_expected = json.load(file)
+        return v1_user_expected, v1_user_actual
+
+    def test_V2_to_V1_tweet_example(self):
+        expected, actual = self._get_expected_actual("V2_tweet.json", "V1_tweet.json")
+        diff = Compare(config=self.compare_config, rules=tweet_exceptions).check(
+            expected, actual
+        )
+        self.assertEqual(diff, NO_DIFF)
+
+    def test_V2_to_V1_tweet_example_1(self):
+        expected, actual = self._get_expected_actual(
+            "V2_tweet_1.json", "V1_tweet_1.json"
+        )
+        diff = Compare(config=self.compare_config, rules=tweet_exceptions).check(
+            expected, actual
+        )
+        self.assertEqual(diff, NO_DIFF)
+
+
+if __name__ == "__main__":
+    unittest.main()
