c178b3fbc53f1b253c007179b8af608556391d54
Mina Remeli <mina.remeli@tuebingen.mpg.de>
2023-02-22 14:20:33 +0100

    Split ALL_TWEET_FIELDS

--- data_collection/collect_utils.py
+++ data_collection/collect_utils.py
@@ -1,4 +1,4 @@
-import logging, os
+import logging, os, sys
 from ssl_smtp_handler import SSLSMTPHandler
 
 ALL_EXPANSIONS = [
@@ -8,7 +8,7 @@ ALL_EXPANSIONS = [
     "attachments.poll_ids",
 ]
 
-ALL_TWEET_FIELDS = [
+TWEET_PUBLIC_FIELDS = [
     "attachments",
     "author_id",
     "context_annotations",
@@ -20,16 +20,24 @@ ALL_TWEET_FIELDS = [
     "geo",
     "in_reply_to_user_id",
     "lang",
-    "non_public_metrics",
-    "organic_metrics",
     "possibly_sensitive",
-    "promoted_metrics",
     "public_metrics",
     "referenced_tweets",
     "reply_settings",
     "source",
     "withheld",
 ]
+
+# Non-public, organic, and promoted metrics are only available for Tweets that have been created within the last 30 days.
+# https://developer.twitter.com/en/docs/twitter-api/metrics
+TWEET_PRIVATE_FIELDS = [
+    "non_public_metrics",
+    "organic_metrics",
+    "promoted_metrics",
+]
+
+ALL_TWEET_FIELDS = TWEET_PUBLIC_FIELDS + TWEET_PRIVATE_FIELDS
+
 
 ALL_USER_FIELDS = [
     "created_at",
