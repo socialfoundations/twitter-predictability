7cfdf2267f665d17c51cec9a6c1557c61a471fe6
Mina Remeli <mina.remeli@tuebingen.mpg.de>
2023-03-23 15:22:09 +0100

    Pass kwargs to wandb.run()

--- data_collection/utils/wandb.py
+++ data_collection/utils/wandb.py
@@ -2,7 +2,7 @@ import wandb
 import os
 
 
-def init_wandb_run(config, job_type, mode="online", log_code=True):
+def init_wandb_run(config, job_type, mode="online", log_code=True, **kwargs):
     run = wandb.init(
         project=os.environ["WANDB_PROJECT"],
         entity="social-foundations",
@@ -10,6 +10,7 @@ def init_wandb_run(config, job_type, mode="online", log_code=True):
         job_type=job_type,
         config=config,
         mode=mode,
+        **kwargs
     )
     if log_code:
         run.log_code()  # save code
