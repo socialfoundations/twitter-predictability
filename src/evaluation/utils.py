0974b9a277354a09c4b42bbfe5661d7ab92475d8
Mina Remeli <mina.remeli@tuebingen.mpg.de>
2023-06-02 11:34:07 +0200

    Change project root path retrieval

--- src/evaluation/utils.py
+++ src/evaluation/utils.py
@@ -1,13 +1,10 @@
+from pyprojroot import here
 from pathlib import Path
 
 
-def get_project_root() -> Path:
-    return Path(__file__).parent.parent.parent
-
-
 def get_prompt_data_path() -> Path:
-    return get_project_root().joinpath("out", "prompt_data")
+    return here().joinpath("out", "prompt_data")
 
 
 def get_prompt_results_path() -> Path:
-    return get_project_root().joinpath("out", "evaluation", "prompt")
+    return here().joinpath("out", "evaluation", "prompt")
