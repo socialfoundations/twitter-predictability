3225e5c9f70b76a9c4e720b68b49c744356ef3b5
Mina Remeli <mina.remeli@tuebingen.mpg.de>
2023-06-15 14:57:58 +0200

    Helper utility for chunking subjects

---
+++ src/evaluation/cluster_utils.py
@@ -0,0 +1,31 @@
+import fire
+from pyprojroot import here
+from utils import get_prompt_data_path
+import os
+import shutil
+
+
+def chunk_subjects(N):
+    all_subjects = os.listdir(get_prompt_data_path())
+
+    def chunks(l, n):
+        """Yield n number of striped chunks from l."""
+        for i in range(0, n):
+            yield l[i::n]
+
+    subject_chunks = chunks(all_subjects, N)
+
+    out_dir = here().joinpath("src", "evaluation", "subjects_chunked")
+    out_dir.mkdir(exist_ok=True)  # create directory
+
+    if any(out_dir.iterdir()):
+        shutil.rmtree(out_dir)  # remove directory and contents
+        out_dir.mkdir(exist_ok=True)  # create directory
+
+    for i, subjects in enumerate(subject_chunks):
+        with open(out_dir.joinpath(f"chunk_{i}.txt"), "w") as f:
+            f.write("\n".join(subjects))
+
+
+if __name__ == "__main__":
+    fire.Fire()
